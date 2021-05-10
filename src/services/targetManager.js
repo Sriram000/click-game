aca6b4eb3187c97766a734d7aded6d2e5235c195
sriram000 <srirammani14@gmail.com>
2021-05-10 21:03:25 +0530

    Refactored.

--- src/services/targetManager.js
+++ src/services/targetManager.js
@@ -1,25 +1,28 @@
 import config from '../core/config';
 import { rndBetween, rndString } from "@laufire/utils/random";
 
+const getRandomX = () => rndBetween(0, 100 - config.width);
+
+const getRandomY = () => rndBetween(0, 100 - config.height);
+
+const getTarget = ({ x, y } = {}) => ({
+    id: rndString(8), 
+    x: x !== undefined ? x : getRandomX(),
+    y: y !== undefined ? y : getRandomY(),
+});
+
+const moveTargets = (targets) => targets.map(() => ({
+    x: getRandomX(),
+    y: getRandomY(),
+}));
+
+const addTarget = (targets) => targets.concat(getTarget());
+
 const TargetManager = {
-
-    moveTargets: (targets) => targets.map(()=> ({
-        x: rndBetween(0, 100 - config.width),
-        y: rndBetween(0, 100 - config.height), 
-    })),
-
-    addTarget: (targets) => targets.concat({
-        id: rndString(8), 
-        x: rndBetween(0, 100 - config.width),
-        y: rndBetween(0, 100 - config.height),
-    }),
-
-    getTarget: ({ x, y }) => ({
-        id: rndString(8), 
-        x: x !== undefined ? x : rndBetween(0, 100 - config.width),
-        y: y !== undefined ? y : rndBetween(0, 100 - config.height),
-    }),
+    moveTargets,
+    addTarget,   
+    getTarget,
 }
 
-
 export default TargetManager;
+
